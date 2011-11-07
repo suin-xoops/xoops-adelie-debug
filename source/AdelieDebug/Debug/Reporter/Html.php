ceefc9a1444cb444b163af85a7a89fbc37dc0a23
Hidehito Nozawa aka Suin <suinyeze@gmail.com>
2011-11-07 14:11:02 +0900

    デバッグ表示時にOBバッファーを全解除するのをやめた fix #3

--- source/AdelieDebug/Debug/Reporter/Html.php
+++ source/AdelieDebug/Debug/Reporter/Html.php
@@ -13,8 +13,6 @@ class AdelieDebug_Debug_Reporter_Html extends AdelieDebug_Debug_Reporter
 {
 	protected $reportable = null;
 
-	protected $hasContent = false;
-
 	public function setUp()
 	{
 		$this->reportable = new AdelieDebug_Debug_Reporter_Html_Reportable();
@@ -27,21 +25,7 @@ class AdelieDebug_Debug_Reporter_Html extends AdelieDebug_Debug_Reporter
 			return;
 		}
 
-		$this->_flushObContents();
 		$this->_printContents();
-	}
-
-	protected function _flushObContents()
-	{
-		$contents = '';
-
-		while ( ob_get_level() > 0 )
-		{
-			$contents .= ob_get_clean();
-			$this->hasContent = true;
-		}
-
-		echo $contents;
 	}
 
 	protected function _printContents()
